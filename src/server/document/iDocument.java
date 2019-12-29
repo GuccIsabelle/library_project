ce491e5ff2e0bef2373968c616a887f32ef742cd
Marius Vallas <vallasmarius@gmail.com>
2019-12-29 22:48:33 +0100

    added little bit of comment and useful description

--- src/server/document/iDocument.java
+++ src/server/document/iDocument.java
@@ -1,10 +1,22 @@
+/**
+ * Sorry but I love adding that lil' "i" before
+ * the interface's name, only because I heard
+ * some professors don't like it. Sorry again.
+ * - Marius Vallas.
+ *
+ * @author Marius Vallas, Gabriel Arbane, Antoine Dedieu
+ * @version 1.1
+ */
+
 package server.document;
 
 import server.user.User;
 
 public interface iDocument {
+    /* books an item for someone */
     void booking(User ab) throws BookingException;
 
+    /* set an item to borrowed */
     void borrowing(User ab) throws BookingException;
 
     /* return document or cancel booking */
