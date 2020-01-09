8dd7a13b0a907609099e9a6b7b47e3f9c9eb9497
Marius Vallas <vallasmarius@gmail.com>
2020-01-09 11:04:31 +0100

    first comment coat applied

--- src/server/Server.java
+++ src/server/Server.java
@@ -17,8 +17,12 @@ import java.util.Objects;
 
 public class Server {
     public static void main(String[] args) throws Exception {
+
+        /* creating the library */
         Library library = new Library("C:\\Users\\Marius\\Documents\\Code\\JAVA\\library_project\\src\\server\\document\\book\\library");
         System.out.println(library.toString());
+
+        /* creating the user database */
         UserDB userDB = new UserDB("C:\\Users\\Marius\\Documents\\Code\\JAVA\\library_project\\src\\server\\user\\database");
         System.out.println(userDB.toString());
 
@@ -26,24 +30,30 @@ public class Server {
          * Booking thread
          */
         new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " started");
             final int port = 2500;
+            /* creating the serverSocket */
             try (ServerSocket serverSocket = new ServerSocket(port)) {
                 while (true) {
                     Socket socket = serverSocket.accept();
+                    System.out.println("connexion established with " + socket.getInetAddress().getHostName());
                     new Thread(() -> {
+                        System.out.println("sub-thread " + Thread.currentThread().getId() + " started");
                         try {
                             /* initializing */
                             DataInputStream inFromClient = new DataInputStream(socket.getInputStream());
                             String userID = inFromClient.readUTF();
                             String bookID = inFromClient.readUTF();
-                            /* processing & outputting */
+                            /* processing */
                             Objects.requireNonNull(library.getCatalog().stream()
                                     .filter(book -> book.getID().equals(bookID))
                                     .findAny().orElse(null))
                                     .booking(userDB.findUserFromID(userID));
+                            /* outputting */
                             new DataOutputStream(socket.getOutputStream()).writeUTF("Booking successful.");
                         } catch (IOException | BookingException e) {
                             e.printStackTrace();
+                            /* sending error message to client */
                             try {
                                 new DataOutputStream(socket.getOutputStream()).writeUTF(e.getMessage());
                             } catch (IOException ex) {
@@ -56,30 +66,36 @@ public class Server {
                 System.out.println("Server not found or some shit...\n" +
                         "IOException error be like: " + e.getMessage());
             }
-        }).start();
+        }, "booking_thread").start();
 
         /**
          * Borrowing thread
          */
         new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " started");
             final int port = 2600;
+            /* creating the serverSocket */
             try (ServerSocket serverSocket = new ServerSocket(port)) {
                 while (true) {
                     Socket socket = serverSocket.accept();
+                    System.out.println("connexion established with " + socket.getInetAddress().getHostName());
                     new Thread(() -> {
+                        System.out.println("sub-thread " + Thread.currentThread().getId() + " started");
                         try {
                             /* initializing */
                             DataInputStream inFromClient = new DataInputStream(socket.getInputStream());
                             String userID = inFromClient.readUTF();
                             String bookID = inFromClient.readUTF();
-                            /* processing & outputting */
+                            /* processing */
                             Objects.requireNonNull(library.getCatalog().stream()
                                     .filter(book -> book.getID().equals(bookID))
                                     .findAny().orElse(null))
                                     .borrowing(userDB.findUserFromID(userID));
+                            /* outputting */
                             new DataOutputStream(socket.getOutputStream()).writeUTF("Borrowing successful.");
                         } catch (IOException | BookingException e) {
                             e.printStackTrace();
+                            /* sending error message to client */
                             try {
                                 new DataOutputStream(socket.getOutputStream()).writeUTF(e.getMessage());
                             } catch (IOException ex) {
@@ -92,28 +108,34 @@ public class Server {
                 System.out.println("Server not found or some shit...\n" +
                         "IOException error be like: " + e.getMessage());
             }
-        }).start();
+        }, "borrowing_thread").start();
 
         /**
          * Returning thread
          */
         new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " started");
             final int port = 2700;
+            /* creating the serverSocket */
             try (ServerSocket serverSocket = new ServerSocket(port)) {
                 while (true) {
                     Socket socket = serverSocket.accept();
+                    System.out.println("connexion established with " + socket.getInetAddress().getHostName());
                     new Thread(() -> {
+                        System.out.println("sub-thread " + Thread.currentThread().getId() + " started");
                         try {
                             /* initializing */
                             String bookID = new DataInputStream(socket.getInputStream()).readUTF();
-                            /* processing & outputting */
+                            /* processing */
                             Objects.requireNonNull(library.getCatalog().stream()
                                     .filter(book -> book.getID().equals(bookID))
                                     .findAny().orElse(null))
                                     .returning();
+                            /* outputting */
                             new DataOutputStream(socket.getOutputStream()).writeUTF("Returning successful.");
                         } catch (IOException | iDocument.ReturnException e) {
                             e.printStackTrace();
+                            /* sending error message to client */
                             try {
                                 new DataOutputStream(socket.getOutputStream()).writeUTF(e.getMessage());
                             } catch (IOException ex) {
@@ -126,19 +148,25 @@ public class Server {
                 System.out.println("Server not found or some shit...\n" +
                         "IOException error be like: " + e.getMessage());
             }
-        }).start();
+        }, "returning_thread").start();
 
         /**
          * Authentication thread
          */
         new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " started");
             final int port = 69; // ah ah lmao, get it ?
+            /* creating the serverSocket */
             try (ServerSocket serverSocket = new ServerSocket(port)) {
                 while (true) {
                     Socket socket = serverSocket.accept();
+                    System.out.println("connexion established with " + socket.getInetAddress().getHostName());
                     new Thread(() -> {
+                        System.out.println("sub-thread " + Thread.currentThread().getId() + " started");
                         try {
+                            /* getting user's credentials */
                             String[] userCredentials = new DataInputStream(socket.getInputStream()).readUTF().split("\\s+");
+                            /* sending out the corresponding user, null is none */
                             new ObjectOutputStream(socket.getOutputStream()).writeObject(userDB.returnIfExist(userCredentials[0], userCredentials[1]));
 
                         } catch (IOException e) {
@@ -151,17 +179,21 @@ public class Server {
                 e.printStackTrace();
             }
 
-        }).start();
+        }, "authentication_thread").start();
 
         /**
          * Catalogue thread
          */
         new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " started");
             final int port = 420; // that's comedy right here
+            /* creating the serverSocket */
             try (ServerSocket serverSocket = new ServerSocket(port)) {
                 while (true) {
                     Socket socket = serverSocket.accept();
+                    System.out.println("connexion established with " + socket.getInetAddress().getHostName());
                     new Thread(() -> {
+                        System.out.println("sub-thread " + Thread.currentThread().getId() + " started");
                         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy 'at' HH:mm:ss z");
                         Date date = new Date(System.currentTimeMillis());
                         try {
@@ -177,6 +209,6 @@ public class Server {
                 e.printStackTrace();
             }
 
-        }).start();
+        }, "catalogue_thread").start();
     }
 }
